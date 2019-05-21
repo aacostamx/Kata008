99cecf120dfe61c82d89649bcf60d030e335997b
Antonio Acosta Murillo <antonio.acosta@4thsource.com>
2019-05-21 09:16:28 -0500

    last version of kata008

--- Kata008.Test/UnitTest1.cs
+++ Kata008.Test/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using Kata008;
+using System.Threading.Tasks;
 
 namespace Kata008.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Get_URL_from_App_Settings()
         {
+            string url = Program.GetURL();
 
+            Assert.NotNull(url);
+        }
+
+
+        [Fact]
+        public void Get_Call_Async()
+        {
+            string url = "https://www.google.com";
+
+            Task result = Program.GetCallAsync(url);
+
+            Assert.NotNull(result);
         }
     }
 }
--- Kata008/Program.cs
+++ Kata008/Program.cs
@@ -1,25 +1,43 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Kata008
 {
-    class Program
+    public static class Program
     {
-        private static HttpClient _client = new HttpClient();
+        private static readonly HttpClient _client = new HttpClient();
         static async Task Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-
             try
             {
-                var result = await _client.GetAsync("https://www.google.com");
-                Console.WriteLine(result.StatusCode);
-            } catch
-            {
-                throw;
+                string url = GetURL();
+                await GetCallAsync(url);
             }
-           
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        public static string GetURL()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot config = builder.Build();
+
+            return config.GetSection("AppConfig").GetSection("url").Value;
+        }
+
+        public static async Task GetCallAsync(string url)
+        {
+            HttpResponseMessage result = await _client.GetAsync(url);
+            Console.WriteLine(result.StatusCode);
         }
     }
 }
