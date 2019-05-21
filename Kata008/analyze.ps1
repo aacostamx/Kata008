795b2b49794f740a1792fd37c90db4a7c413aea7
Antonio Acosta Murillo <antonio.acosta@4thsource.com>
2019-05-21 08:41:23 -0500

    add my username

--- Kata008/analyze.ps1
+++ Kata008/analyze.ps1
@@ -1,3 +1,3 @@
 ﻿dotnet sonarscanner begin /k:"<<YOUR PROJECT KEY>>" /d:sonar.login="sonar_user" /d:sonar.password="pass@word1" /d:sonar.host.url="https://atinternalsonar.azurewebsites.net"
 dotnet build
-dotnet sonarscanner end  /d:sonar.login="sonar_user" /d:sonar.password="pass@word1"
+dotnet sonarscanner end  /d:sonar.login="kata008_aacosta" /d:sonar.password="pass@word1"
